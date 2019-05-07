c8890e60983b742242ce466bc20d182073de7867
dsanchez98 <44051330+dsanchez98@users.noreply.github.com>
2019-05-07 09:08:21 +0200

    Hecho por Dani

---
+++ src/maquinabebidas4_2019/DispensadorDeBotes.java
@@ -0,0 +1,36 @@
+
+/** 
+ * @author Daniel Sanchez
+ */
+class DispensadorDeBotes
+{
+private int botes; 
+//Numero actual de botes.
+/* Inicializa el dispensador y pone i botes en él*/
+/* Entrada: int i, numero de botes en el dispensador*/
+/* Salidas: Ninguna */
+public void iniciarDispensadorDeBotes (int i){
+    botes = i;
+};
+/* 
+* Intenta dar un bote, devuelve true si es posible y falso
+* si no hay botes. 
+* Entrada: Ninguna 
+* Salidas: bool, true si entrega el bote */
+public boolean pulsarBoton (){
+    boolean hayBotes = false;
+    if(botes>0){
+        hayBotes= true;
+        botes--;
+    }
+    else
+        hayBotes= false;
+    return hayBotes;
+};
+/*Muestra el numero de botes disponibles en el dispensador*/
+/* Entrada: Ninguna */
+/* Salidas: int, el numero de botes */
+public int botesDisponibles (){
+    return botes;
+};
+}
